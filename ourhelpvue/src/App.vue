8ef5bb429837075946b8971bbcec86618af1237c
mmasso <mmasso@cifpfbmoll.eu>
2021-04-16 21:11:02 +0200

    feat: :sparkles: app and home

--- ourhelpvue/src/App.vue
+++ ourhelpvue/src/App.vue
@@ -1,56 +1,18 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <Menu/>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+
+import Menu from "./components/menu"
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Menu
   },
 
   data: () => ({
@@ -58,3 +20,15 @@ export default {
   }),
 };
 </script>
+
+<style>
+  .v-window-item { 
+    color: black}
+  .v-sheet.v-card {
+    /* border-radius: 4px; */
+    border-radius: 0px !important;
+  }
+  .v-application{
+
+    }
+</style>
--- ourhelpvue/src/views/Home.vue
+++ ourhelpvue/src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container class="grey lighten-5">
+    <v-row no-gutters>
+      <v-col cols="12" sm="6" md="8" lg="4">
+        <v-card class="pa-2 mx-2" style="height: 100%;">
+          <h2>Our Help: Lorem Ipsum</h2>
+          <br/>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ea doloremque nobis nemo culpa perferendis voluptas! Ut nesciunt rerum tempore quod voluptate? Voluptatum omnis consectetur nulla deserunt aspernatur reprehenderit nemo.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, praesentium accusamus ab deleniti obcaecati perferendis id ipsum corporis nulla exercitationem minima sit! Quo laborum perspiciatis similique quibusdam, possimus illo alias.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem tenetur, id mollitia labore sed quaerat nihil impedit magni. Dignissimos earum temporibus, sed aliquam neque ut porro perferendis obcaecati repudiandae necessitatibus?</p>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6" md="4" lg="8">
+        <v-card class="pa-2  mx-2" style="height: 100%;" ><Carrousel/></v-card>
+      </v-col>
+    </v-row>
+  
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import Carrousel from "../components/carrousel";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Carrousel,
+  },
+};
 </script>
